5dab7cb2c89d75c05428c5862f77dcea8572b602
hale.huo <hale.huo@zhangmen.com>
2021-07-08 15:06:07 +0800

    代码优化

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,14 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message)
     // 这里是返回给bg的内容
-    var trs = $('.ant-table-tbody tr td:nth-child(2)');
+    var trs = $('.ant-table-tbody tr');
     var size = trs.size();
     var newTrs = $.makeArray(trs).splice(size - 50, 50)
     newTrs.forEach(element => {
-        var td = $(element).find(".editCell_f2UJB > .content_3-COB > .formatted_2iUZF")
-        console.log(td.html());
-        td.after('<img src="https://hopefully-img.yuedun.wang/' + td.html() + '?imageMogr2/thumbnail/!20p/" style="width:100px;">')
+        var tdKey = $(element).data("row-key")
+        console.log(tdKey);
+        var filenane = tdKey.substr(4);
+        $(element).find("td:nth-child(2) > .editCell_f2UJB > .content_3-COB > .formatted_2iUZF").before('<img src="https://hopefully-img.yuedun.wang/' + filenane + '?imageMogr2/thumbnail/!20p/" style="width:100px;">')
     });
     sendResponse('get the message')
 })
